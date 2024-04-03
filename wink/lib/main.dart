2adc38da8cbe7976f879dd6fe78ebaf926f5847b
Diana Mircea <mirceadiana502@gmail.com>
2024-04-03 06:44:32 +0300

    sa moara eu

--- wink/lib/main.dart
+++ wink/lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:wink/responsive/mobile_screen_layout.dart';
 import 'package:wink/responsive/responsive_layout_screen.dart';
 import 'package:wink/responsive/web_screen_layout.dart';
 import 'package:wink/utils/colors.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(apiKey: 'AIzaSyAmDTuCbcBS-a9MGPV6prdfeGofN7HUhTI', 
+      appId: '1:340104578704:web:e0fdf7e624969f931bfa15', 
+      messagingSenderId: '340104578704', 
+      projectId: 'wink-c3240',
+      storageBucket: 'wink-c3240.appspot.com',
+    ),
+    );
+  }else{
+  await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
